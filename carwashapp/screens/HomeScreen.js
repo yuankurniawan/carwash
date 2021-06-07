b482c19465a0ba7f043b54b981547fad040ceb87
wirayuda24 <45593381+wirayuda24@users.noreply.github.com>
2021-06-07 19:47:51 +0700

    Update

---
+++ carwashapp/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return(
+            <MapView style={styles.container}
+                initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+                }}
+            />
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+})
